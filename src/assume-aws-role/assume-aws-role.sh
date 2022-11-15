7f0e9aabf9898ecf099323bdf91398e21b66b535
Christian Nuss <christian.nuss@gmail.com>
2022-11-15 08:25:30 -0800

    updates + rename to aws-assume-role

---
+++ src/assume-aws-role/assume-aws-role.sh
@@ -0,0 +1,61 @@
+#!/bin/sh
+set -e
+
+GITHUB_TOKEN=$(cat /workspaces/.codespaces/shared/.env | grep GITHUB_TOKEN | sed "s/GITHUB_TOKEN=//1")
+
+if [ -z "${GITHUB_TOKEN}" ]; then
+    echo "Error: GITHUB_TOKEN is not set" >&2
+    exit 1
+fi
+
+ROLE="$(cat /workspaces/.codespaces/shared/.saml-to/aws-role)"
+
+if [ -z "${ROLE}" ]; then
+    echo "Error: aws-role is not set" >&2
+    exit 1
+fi
+
+PROFILE="$(cat /workspaces/.codespaces/shared/.saml-to/aws-profile)"
+
+if [ -z "${PROFILE}" ]; then
+    echo "Error: aws-profile is not set" >&2
+    exit 1
+fi
+
+REGION="$(cat /workspaces/.codespaces/shared/.saml-to/aws-region)"
+
+if [ -z "${REGION}" ]; then
+    echo "Error: aws-region is not set" >&2
+    exit 1
+fi
+
+ROLE_ENCODED=$(echo "${ROLE}" | jq -Rr @uri)
+
+samlRequest=$(curl -X POST -H "accept: application/json" -A "devcontaier-features/assume-aws-role" -H "Authorization: Bearer ${GITHUB_TOKEN}" "https://sso.saml.to/github/api/v1/idp/roles/${ROLE_ENCODED}/assume" 2>/dev/null | jq -r .samlHttpRequest)
+
+content_type=$(echo "${samlRequest}" | jq -r .contentType)
+url=$(echo "${samlRequest}" | jq -r .url)
+method=$(echo "${samlRequest}" | jq -r .method)
+payload=$(echo "${samlRequest}" | jq -r .payload)
+
+samlResponse=$(curl -X "${method}" -H "Accept: application/json" -H "Content-Type: ${content_type}" --data "${payload}" "${url}" 2>/dev/null )
+
+accessKeyId=$(echo "${samlResponse}" | jq -r .AssumeRoleWithSAMLResponse.AssumeRoleWithSAMLResult.Credentials.AccessKeyId)
+secretAccessKey=$(echo "${samlResponse}" | jq -r .AssumeRoleWithSAMLResponse.AssumeRoleWithSAMLResult.Credentials.SecretAccessKey)
+sessionToken=$(echo "${samlResponse}" | jq -r .AssumeRoleWithSAMLResponse.AssumeRoleWithSAMLResult.Credentials.SessionToken)
+
+AWS_DIR="${HOME}/.aws"
+AWS_CREDENTIALS_FILE="${AWS_DIR}/credentials"
+AWS_CONFIG_FILE="${AWS_DIR}/config"
+
+mkdir -p "${AWS_DIR}"
+
+echo "[${PROFILE}]" > "${AWS_CREDENTIALS_FILE}"
+echo "aws_access_key_id = ${accessKeyId}" >> "${AWS_CREDENTIALS_FILE}"
+echo "aws_secret_access_key = ${secretAccessKey}" >> "${AWS_CREDENTIALS_FILE}"
+echo "aws_session_token = ${sessionToken}" >> "${AWS_CREDENTIALS_FILE}"
+
+echo "[${PROFILE}]" > "${AWS_CONFIG_FILE}"
+echo "region = ${REGION}" >> "${AWS_CONFIG_FILE}"
+
+echo "Refreshed Credentials for Role: ${ROLE}"
---
+++ src/assume-aws-role/install.sh
@@ -0,0 +1,58 @@
+#!/bin/sh
+set -e
+
+echo "Setting up AWS credentials for role ${ROLE}..."
+
+if [ -z "${ROLE}" ]; then
+  echo "Error: role is required"
+  exit 1
+fi
+
+if [ -z "${REGION}" ]; then
+  REGION="us-east-1"
+fi
+
+if [ -z "${PROFILE}" ]; then
+  PROFILE="default"
+fi
+
+mkdir -p /workspaces/.codespaces/shared/.saml-to
+chmod +r /workspaces/.codespaces/shared/.saml-to
+
+echo "${ROLE}" > /workspaces/.codespaces/shared/.saml-to/aws-role
+chmod +r /workspaces/.codespaces/shared/.saml-to/aws-role
+
+echo "${REGION}" > /workspaces/.codespaces/shared/.saml-to/aws-region
+chmod +r /workspaces/.codespaces/shared/.saml-to/aws-region
+
+echo "${PROFILE}" > /workspaces/.codespaces/shared/.saml-to/aws-profile
+chmod +r /workspaces/.codespaces/shared/.saml-to/aws-profile
+
+cp assume.sh /usr/local/bin/assume-aws-role.sh
+chmod +rx /usr/local/bin/assume-aws-role.sh
+
+echo "Installing Profile Script..."
+
+# Assumption at session start
+# cat <<EOT >> /etc/profile.d/01-assume-aws-role.sh
+# #!/bin/sh
+# /usr/local/bin/assume-aws-role.sh
+# EOT
+# chmod +rx /etc/profile.d/01-assume-aws-role.sh
+
+echo "Setting up Credential Refreshes using Cron..."
+
+# Re-assumption using Cron
+# TODO switch to use credentials from /workspaces/.codespaces/shared/.env-secrets
+# TODO set credentials globally for all users, unhardcode 'codespace'
+# TODO switch to every 30 minutes
+cat <<EOT >> /etc/cron.d/assume-aws-role
+* * * * * root sudo -S -i -u codespace /usr/local/bin/assume-aws-role.sh
+EOT
+chmod +r /etc/cron.d/assume-aws-role
+
+echo "Installing Cron..."
+# TODO use saml-to cli in daemon mode
+apt-get update
+apt-get -y install --no-install-recommends cron
+rm -rf /var/lib/apt/lists/*
